7e1e385d2854723eaf96949a6c78de4a50fb18e0
xAndresCr <andresscastilloo81@gmail.com>
2026-02-18 00:52:24 -0600

    Agregación de nav menu

---
+++ SubastaAutos.Application/DTOs/UsuarioDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubastaAutos.Application.DTOs
+{
+    public record UsuarioDTO
+    {
+        public int IdUsuario { get; set; }
+
+        public string Correo { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string NombreCompleto { get; set; } = null!;
+
+        public int IdRol { get; set; }
+
+        public int IdEstadoUsuario { get; set; }
+    }
+}
