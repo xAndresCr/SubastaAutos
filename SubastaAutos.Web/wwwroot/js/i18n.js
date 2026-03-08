1d0813c05fe0c4a255ad405f2ee988f5f1bb6f6c
ANDRES MAURICIO CASTILLO CRUZ <ancastillocr@est.utn.ac.cr>
2026-03-08 17:14:46 -0600

    prueba de traducción con I18next

---
+++ SubastaAutos.Web/wwwroot/js/i18n.js
@@ -0,0 +1,13 @@
+﻿i18next
+    .use(i18nextHttpBackend)
+    .init({
+        lng: "en",
+        fallbackLng: "en",
+        backend: {
+            loadPath: "/locales/{{lng}}/translation.json"
+        }
+    }, function () {
+        document.querySelectorAll("[data-i18n]").forEach(function (element) {
+            element.innerHTML = i18next.t(element.getAttribute("data-i18n"));
+        });
+    });
