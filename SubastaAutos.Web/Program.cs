a800acc19125b8814ff36f31552903e44e2a42ee
ANDRES MAURICIO CASTILLO CRUZ <ancastillocr@est.utn.ac.cr>
2026-04-07 00:51:12 -0600

    Signal R instalado

--- SubastaAutos.Web/Program.cs
+++ SubastaAutos.Web/Program.cs
@@ -9,8 +9,25 @@ using SubastaAutos.Infraestructure.Repository.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Cache en memoria (requerido por Session)
+builder.Services.AddDistributedMemoryCache();
+
+//Registrar Session con opciones
+builder.Services.AddSession(options =>
+{
+    // Tiempo m�ximo sin actividad antes de expirar la sesi�n
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+
+    // Opciones de la cookie de sesi�n
+    options.Cookie.HttpOnly = true;          // No accesible desde JS
+    options.Cookie.IsEssential = true;       // Necesaria aunque haya consentimiento de cookies
+    options.Cookie.Name = ".Libreria.Session";
+});
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 //***********
 // =======================
